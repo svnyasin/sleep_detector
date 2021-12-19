85d88373f8fb8b36f36ed1e558f7030fd7327ba1
Yasin Seven <67184421+svnyasin@users.noreply.github.com>
2021-12-19 21:16:06 +0300

    .

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -1,6 +1,10 @@
 import face_recognition
 import cv2
+import os
+from keras.backend import print_tensor
 import numpy as np
+from keras.models import load_model
+from pygame import mixer
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
@@ -11,25 +15,48 @@ import numpy as np
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
+leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+
+lbl=['Close','Open']
+
+model = load_model('models/cnncat2.h5')
+path = os.getcwd()
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count=0
+score=0
+thicc=2
+rpred=[99]
+lpred=[99]
+
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a first sample picture and learn how to recognize it.
+yasin_seven_image = face_recognition.load_image_file("yasin_seven.jpg")
+yasin_seven_face_encoding = face_recognition.face_encodings(yasin_seven_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+ogulcan_galata_image = face_recognition.load_image_file("ogulcan_galata.jpeg")
+ogulcan_galata_face_encoding = face_recognition.face_encodings(ogulcan_galata_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
+    yasin_seven_face_encoding,
+    ogulcan_galata_face_encoding
+
+
 ]
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
+    "Yasin Seven",
+    "Ogulcan Galata"
+
 ]
 
 # Initialize some variables
@@ -41,6 +68,12 @@ process_this_frame = True
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
+    height,width = frame.shape[:2] 
+
+
+    left_eye = leye.detectMultiScale(frame)
+    right_eye =  reye.detectMultiScale(frame)
+    print(right_eye)
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -85,12 +118,75 @@ while True:
         left *= 4
 
         # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom ), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.7, (255, 255, 255), 1)
+
+
+        for (x,y,w,h) in right_eye:
+            r_eye=frame[y:y+h,x:x+w]
+            count=count+1
+            r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+            r_eye = cv2.resize(r_eye,(24,24))
+            r_eye= r_eye/255
+            r_eye=  r_eye.reshape(24,24,-1)
+            r_eye = np.expand_dims(r_eye,axis=0)
+            rpred_x = model.predict(r_eye)
+            rpred=np.argmax(rpred_x,axis=1)
+            if(rpred[0]==1):
+                lbl='Open' 
+            if(rpred[0]==0):
+                lbl='Closed'
+            break
+
+        for (x,y,w,h) in left_eye:
+            l_eye=frame[y:y+h,x:x+w]
+            count=count+1
+            l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+            l_eye = cv2.resize(l_eye,(24,24))
+            l_eye= l_eye/255
+            l_eye=l_eye.reshape(24,24,-1)
+            l_eye = np.expand_dims(l_eye,axis=0)
+            lpred_x = model.predict(l_eye)
+            lpred=np.argmax(lpred_x,axis=1)
+            if(lpred[0]==1):
+                lbl='Open'   
+            if(lpred[0]==0):
+                lbl='Closed'
+            break
+
+        if(rpred[0]==0 and lpred[0]==0):
+            if (score <= 25):
+                score=score+1
+            print(name, " ", str(score))
+        # if(rpred[0]==1 or lpred[0]==1):
+        else:
+            score=score-1
+            print(name, " ", str(score))
+
+            
+        if(score<0):
+            score=0   
+        if(score>24):
+            #person is feeling sleepy so we beep the alarm
+            cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+            try:
+                #sound.play()
+                print("uyuma")
+                
+            except:  # isplaying = False
+                pass
+            if(thicc<16):
+                thicc= thicc+2
+            else:
+                thicc=thicc-2
+                if(thicc<2):
+                    thicc=2
+            cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
+        
 
     # Display the resulting image
     cv2.imshow('Video', frame)
